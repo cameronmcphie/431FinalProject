de6ef883e3e57e44431d209980322282c80774a5
Cameron McPhie <cameronmcphie@Camerons-MBP.socal.rr.com>
2018-05-09 11:43:25 -0700

    Updated to reflect the new ERD

--- CsufBasketballDDL.sql
+++ CsufBasketballDDL.sql
@@ -6,58 +6,68 @@ grant select, insert, delete, update, execute on CsufBasketBall.* to 'phpWebEngi
 
 USE CsufBasketball;
 
-CREATE TABLE Staff
+CREATE TABLE Person
 (
-  StaffId INTEGER NOT NULL AUTO_INCREMENT,
-  UserName VARCHAR(100) NOT NULL,
-  Password VARCHAR(100) NOT NULL,
-  FirstName VARCHAR(30) NOT NULL,
-  LastName VARCHAR(30) NOT NULL,
-
-  UNIQUE(StaffId, Password)
-);
-
-CREATE TABLE Players
-(
-  PlayerId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   FirstName VARCHAR(30),
   LastName VARCHAR(30),
   Street VARCHAR(250),
   City VARCHAR(100),
   Country VARCHAR(100),
   Zipcode CHAR(10),
-  Active BOOLEAN NOT NULL,
-  LastModifiedBy INTEGER UNSIGNED NOT NULL,
-
-  FOREIGN KEY (LastUpdatedBy) REFERENCES Staff(StaffId),
+  Email VARCHAR(100)
 
   CHECK (ZipCode REGEXP '(?!0{5})(?!9{5})\\d{5}(-(?!0{4})(?!9{4})\\d{4})?')
 );
 
-CREATE TABLE Games
-(
-  GameId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  WonGame BOOLEAN NOT NULL,
-  OpposingTeam VARCHAR(50) NOT NULL,
-  OpposingTeamScore TINYINT(3) NOT NULL,
-  LastUpdatedBy INTEGER NOT NULL,
 
-  FOREIGN KEY (LastUpdatedBy) REFERENCES Staff(StaffId)
+CREATE TABLE User
+(
+  PersonId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  Username VARCHAR(100) NOT NULL,
+  Password VARCHAR(100) NOT NULL,
+  Role TINYINT(1) NOT NULL,
+
+  FOREIGN KEY (PersonId) REFERENCES Person(Id),
+
+  UNIQUE(Username, Password)
 );
 
-CREATE TABLE StatsPerGame (
-  StatId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  Player INTEGER UNSIGNED NOT NULL,
-  Game INTEGER UNSIGNED NOT NULL,
-  Playertimemin TINYINT(2)  UNSIGNED DEFAULT 0,
-  Playertimesec TINYINT(2) UNSIGNED DEFAULT 0,
+CREATE TABLE Player
+(
+  PersonId INTEGER UNSIGNED NOT NULL PRIMARY KEY,
+  Active BOOLEAN NOT NULL,
+  Height TINYINT(3) DEFAULT 0,
+  Weight TINYINT(3) DEFAULT 0,
+  InactiveNote VARCHAR(300),
+  LastModifiedBy INTEGER UNSIGNED NOT NULL,
+
+  FOREIGN KEY (PersonId) REFERENCES Person(Id)
+);
+
+
+CREATE TABLE Games
+(
+  Id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  WonGame BOOLEAN NOT NULL,
+  OpposingTeam VARCHAR(100) NOT NULL,
+  OpposingTeamScore TINYINT(3) NOT NULL,
+  LastUpdatedBy INTEGER NOT NULL
+);
+
+CREATE TABLE StatsPerGame
+(
+  PlayerId INTEGER UNSIGNED NOT NULL,
+  GameId INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  TimeMin TINYINT(2)  UNSIGNED DEFAULT 0,
+  TimeSec TINYINT(2) UNSIGNED DEFAULT 0,
   Points TINYINT UNSIGNED DEFAULT 0,
   Assists TINYINT UNSIGNED DEFAULT 0,
   Rebounds TINYINT UNSIGNED DEFAULT 0,
 
-  FOREIGN KEY (Player) REFERENCES Players(PlayerId),
-  FOREIGN KEY (Game) REFERENCES Games(GameId),
+  FOREIGN KEY (PlayerId) REFERENCES Player(PersonId),
+  FOREIGN KEY (GameId) REFERENCES Games(Id),
 
-  CHECK((PlayingTimeMin < 40 AND PlayingTimeSec < 60) OR
-        (PlayingTimeMin = 40 AND PlayingTimeSec = 0 ))
+  CHECK((TimeMin < 40 AND TimeSec < 60) OR
+        (TimeMin = 40 AND TimeSec = 0 ))
 );
