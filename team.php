23df11f193622225822a86b98ee74704dd3f9e0b
Cameron McPhie <cameronmcphie@mramella.convergence.net>
2018-05-13 08:46:22 -0700

    echo session data

--- team.php
+++ team.php
@@ -9,6 +9,20 @@
   // Show on the left games with opponent and score that links to the game stats/boxscore
   // Show on the right the team roster try to sort by active and inactive players and show if they are avtive or inactive
   // This should link to a page that shows the individual player with his game stats
+
+  echo $_SESSION['valid_user'];
+  echo $_SESSION['user_id'];
+  echo $_SESSION['role'];
+?>
+
+
+
+
+<?php
+  require_once('funtions/html_base.php');
+  do_footer();
+ ?>
+
 ?>
 
 
